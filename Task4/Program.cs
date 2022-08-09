d1e40945ac9c953788633df29e919422bc2fff4d
Indira Ivleva <indira.ivleva@gmail.com>
2022-08-09 22:07:41 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число N:");
+int n = Convert.ToInt32(ReadLine());
+int i = 1;
+WriteLine($"Ряд четных чисел от 1 до {n}");
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+        
+        Write(i);
+        Write(", ");
+        i++;
+    }
+
+    else
+    {
+        i++;
+    }
+}
